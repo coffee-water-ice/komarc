4f53d9b9953fa9675c82cc78d077297f5cb3f3cc
coffeeiced <koop145@gmail.com>
2025-09-28 08:39:13 +0900

    Update 웹크롤링1.py

--- 웹크롤링1.py
+++ 웹크롤링1.py
@@ -368,7 +368,7 @@ def get_mcst_address(publisher_name):
             return "미확인", [], debug_msgs
     except Exception as e:
         debug_msgs.append(f"[문체부] 예외 발생: {e}")
-        return "발생 오류", [], debug_msgs
+        return "발생 [오류]", [], debug_msgs
 
         
 # =========================
